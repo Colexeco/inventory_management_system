858286f359502b5e98178f23dd8e3a59c750a415
colexeco <evan.shoemaker0@gmail.com>
2024-07-30 23:00:28 -0700

    add item popup

--- inventory_management_system/app/page.js
+++ inventory_management_system/app/page.js
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import { useState, useEffect } from 'react'
 import {firestore} from '@/firebase'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Modal, Stack, TextField, Button} from '@mui/material'
 
 export default function Home() {
   const [inventory, setInventory] = useState([])
@@ -57,10 +57,57 @@ export default function Home() {
     updateInventory()
   }, [])
 
-  handleOpen = () => setOpen(true)
-  handleClose = () => setOpen(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
 
-  return <Box>
-      <Typography variant="h1">Inventory Management</Typography>
+  return (
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display="flex" 
+      justifyContent="center" 
+      alignItems={"center"} 
+      gap={2}
+    >
+      <Modal
+        open={open}
+        onClose={handleClose}
+      >
+        <Box
+          position={'absolute'}
+          top="50%"
+          left="50%"
+          width={400}
+          bgcolor="white"
+          border="2px solid #000"
+          boxShadow={24}
+          p={4}
+          display={"flex"}
+          flexDirection={"column"}
+          gap={3}
+          sx={{
+            transform:"translate(-50%,-50%)"
+          }}
+        >
+          <Typography variant="h6">Add Item</Typography>
+          <Stack width="100%" direction="row" spacing={2}>
+            <TextField
+            variant="outlined"
+            fullWidth
+            value={itemName}
+            onChange={(e) => {
+              setItemName(e.target.value)
+            }}
+            ></TextField>
+            <Button variant="outlined" onClick={()=>{
+              addItem(itemName)
+              setItemName('')
+              handleClose()
+            }}>Add</Button>
+          </Stack>
+        </Box>
+      </Modal>
+        <Typography variant="h1">Inventory Management</Typography>
     </Box>
+  )
 }
