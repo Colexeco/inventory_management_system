a66583881754b08e4258de3a0ac26000d5a21712
colexeco <evan.shoemaker0@gmail.com>
2024-07-31 19:26:04 -0700

    prepare for vercel deployment

---
+++ inventory_management_system/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "inventory-management-sys-f8870.firebaseapp.com",
+  projectId: "inventory-management-sys-f8870",
+  storageBucket: "inventory-management-sys-f8870.appspot.com",
+  messagingSenderId: "322811984085",
+  appId: process.env.APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const firestore = getFirestore(app)
+export {firestore}
