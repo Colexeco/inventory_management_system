a05395e4b7a090039d7c86abd7c617fe0dcb734a
colexeco <evan.shoemaker0@gmail.com>
2024-07-31 19:51:12 -0700

    remake firebase db

--- inventory_management_system/firebase.js
+++ inventory_management_system/firebase.js
@@ -1,20 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1TIXHCKcnLwwa-ETFWotZ-QmDPIpLGNc",
+  apiKey: process.env.API_KEY,
   authDomain: "inventory-management-sys-f8870.firebaseapp.com",
   projectId: "inventory-management-sys-f8870",
   storageBucket: "inventory-management-sys-f8870.appspot.com",
   messagingSenderId: "322811984085",
-  appId: "1:322811984085:web:84ce5431eff991cf098a5c"
+  appId: process.env.APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app)
 export {firestore}
